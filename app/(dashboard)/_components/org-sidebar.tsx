d6163389f95f82bedf87acda9a96d3486be53aa7
Harsh <57228178+harsh3294@users.noreply.github.com>
2024-02-03 12:57:53 +0530

    feat(dashboard): created the  outline for the dashboard page.

---
+++ app/(dashboard)/_components/org-sidebar.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+});
+
+export const OrgSidebar = () => {
+  return (
+    <div className="hidden lg:flex flex-col space-y-6 w-[206px] pl-5 pt-5">
+      <Link href="/">
+        <div className="flex items-center gap-x-2">
+          <Image src="/logo.svg" alt="Logo" height={60} width={60} />
+          <span className={cn("font-semibold text-2xl", font.className)}>
+            Board
+          </span>
+        </div>
+      </Link>
+    </div>
+  );
+};
---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+const DashboardPage = () => {
+  return (
+    <div className="flex-1 h-[calc(100%-80px)] p-6">Hi I am root dashboard</div>
+  );
+};
+
+export default DashboardPage;
