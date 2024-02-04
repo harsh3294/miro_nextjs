369d15a521ac2db7bafe621199d35ee0722b0775
Harsh <57228178+harsh3294@users.noreply.github.com>
2024-02-04 20:40:23 +0530

    feat(board room): showing all the participants that are there currently using in the same board.

--- app/board/[boardId]/_components/participants.tsx
+++ app/board/[boardId]/_components/participants.tsx
@@ -1,9 +1,45 @@
 "use client";
 
+import { useOthers, useSelf } from "@/liveblocks.config";
+import { UserAvatar } from "./user-avatar";
+import { getRandomColor } from "@/lib/utils";
+
+const MAX_SHOWN_OTHER_USERS = 2;
+
 export const Participants = () => {
+  const users = useOthers();
+  const currentUser = useSelf();
+  const hasMoreUsers = users.length > MAX_SHOWN_OTHER_USERS;
   return (
     <div className="absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md">
-      <div className="flex gap-x-2"></div>
+      <div className="flex gap-x-2">
+        {users.slice(0, MAX_SHOWN_OTHER_USERS).map(({ connectionId, info }) => {
+          return (
+            <UserAvatar
+              borderColor={getRandomColor()}
+              key={connectionId}
+              src={info?.picture}
+              name={info?.name}
+              fallback={info?.name?.[0] || "A"}
+            />
+          );
+        })}
+
+        {currentUser && (
+          <UserAvatar
+            borderColor={getRandomColor()}
+            src={currentUser.info?.picture}
+            name={`${currentUser.info?.name} (You)`}
+            fallback={currentUser.info?.name?.[0]}
+          />
+        )}
+        {hasMoreUsers && (
+          <UserAvatar
+            name={`${users.length - MAX_SHOWN_OTHER_USERS} more`}
+            fallback={`+${users.length - MAX_SHOWN_OTHER_USERS}`}
+          />
+        )}
+      </div>
     </div>
   );
 };
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,21 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+const COLORS = ["#DC2626", "#D97706", "#059669", "#7C3AED", "#DB2777"];
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function connectionIdToColor(connectionId: number): string {
+  return COLORS[connectionId % COLORS.length];
+}
+
+export function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
