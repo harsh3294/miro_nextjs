ac937002338dbfbe70d1c138411a722a9339644d
Harsh <57228178+harsh3294@users.noreply.github.com>
2024-02-04 19:58:02 +0530

    feat(room): added the room authentication to access the board.

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -4,11 +4,13 @@ import { Info } from "./info";
 import { Participants } from "./participants";
 import { SelectionTools } from "./selection-tools";
 import { Toolbar } from "./toolbar";
-
+import { useSelf } from "@/liveblocks.config";
 interface CanvasPProps {
   boardId: string;
 }
 export const Canvas = ({ boardId }: CanvasPProps) => {
+  const info = useSelf((me) => me.info);
+  console.log({ info });
   return (
     <main className="h-full w-full relative bg-neutral-100 touch-none">
       <Info boardId={boardId} />
