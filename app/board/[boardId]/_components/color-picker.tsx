f9ab2c47b01c765b04c36fdd7fd9451a24d81c48
Harsh <57228178+harsh3294@users.noreply.github.com>
2024-03-02 21:01:56 +0530

    fix: added the hint when cursor is hovered to the color picker button.

--- app/board/[boardId]/_components/color-picker.tsx
+++ app/board/[boardId]/_components/color-picker.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Color, Position } from "@/types/canvas";
 import { ColorPicker as ReactColorPicker, themes } from "react-pick-color";
+import { Hint } from "@/components/hint";
 
 interface ColorPickerProps {
   onChange: (color: Color) => void;
@@ -17,7 +18,6 @@ export const ColorPicker = ({
   visible,
   toggleColorPicker,
 }: ColorPickerProps) => {
-  const [showColorPicker, setShowColorPicker] = useState(false);
   const [selectedColor, setSelectedColor] = useState(selectionColor);
 
   const [position, setPosition] = useState<Position>({
@@ -38,14 +38,16 @@ export const ColorPicker = ({
 
   return (
     <div className="flex flex-wrap gap-2 items-center max-w-[164px] pr-2 mr-2 border-r border-neutral-200 relative">
-      <button
-        className=" w-8 h-8 rounded-full border border-gray-400 flex items-center justify-center relative"
-        onClick={toggleColorPicker}
-        style={{
-          background:
-            "conic-gradient(#ff0000, #ff0000 60deg, #00ff00 60deg, #00ff00 120deg, #0000ff 120deg, #0000ff 180deg, #ffff00 180deg, #ffff00 240deg, #800080 240deg, #800080 300deg, #00ffff 300deg, #00ffff)",
-        }}
-      />
+      <Hint label="Color Picker">
+        <button
+          className=" w-8 h-8 rounded-full border border-gray-400 flex items-center justify-center relative"
+          onClick={toggleColorPicker}
+          style={{
+            background:
+              "conic-gradient(#ff0000, #ff0000 60deg, #00ff00 60deg, #00ff00 120deg, #0000ff 120deg, #0000ff 180deg, #ffff00 180deg, #ffff00 240deg, #800080 240deg, #800080 300deg, #00ffff 300deg, #00ffff)",
+          }}
+        />
+      </Hint>
       {visible && (
         <div className="absolute z-10" style={position}>
           <ReactColorPicker
